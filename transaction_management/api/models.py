bdf483762312884cd7007d9630bfb67918bb850e
Val Estrabo <vamestrabo@gmail.com>
2025-04-08 13:13:10 +0800

    Fix and migrate model

--- transaction_management/api/models.py
+++ transaction_management/api/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator
 class TransactionModel(models.Model):
     transaction_type = models.CharField(max_length=10)
     description = models.TextField()
-    amount = models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)])
+    amount = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0.01)])
 
     def __str__(self):
         return self.description
