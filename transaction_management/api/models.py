5caf9f18af8d5f4e616c529ae156dc78c1f11fc5
Val Estrabo <vamestrabo@gmail.com>
2025-04-08 12:46:28 +0800

    Create transaction model

--- transaction_management/api/models.py
+++ transaction_management/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class TransactionModel(models.Model):
+    transaction_type = models.CharField(max_length=10)
+    description = models.TextField()
+    amount = models.DecimalField(decimal_places=2, validators=[MinValueValidator(0.01)])
+
+    def __str__(self):
+        return self.description
